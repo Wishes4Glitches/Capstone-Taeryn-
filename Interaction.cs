04c9c45c66f60b2f16173be94a0e90a37edd832e
Taeryn Denhertog <taeryn@hotmail.ca>
2018-12-12 23:00:17 -0500

    Add files via upload

---
+++ Interaction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Person")
+        {
+            Destroy(col.gameObject);
+        }
+	}
+}
