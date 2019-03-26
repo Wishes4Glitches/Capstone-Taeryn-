cbb61a69b4f412a96713d308288f19bde7682d17
Taeryn Denhertog <taeryn@hotmail.ca>
2019-03-26 18:38:23 -0400

    Update - 3/26/2019

---
+++ CamMouseLook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private string LookX, LookY;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    void Awake()
+    {
+        xAxisClamp = 0.0f;
+    }
+
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    // Update is called once per frame
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxisRaw(LookX) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw(LookY) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,25 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public float speed;
+    public float speed = 10.0f;
 
-    private Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    // Update is called once per frame
+    void Update()
+    {
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
 
-        rb.AddForce (movement * speed);
+        moveHorizontal *= Time.deltaTime;
+        moveVertical *= Time.deltaTime;
+
+        transform.Translate(moveHorizontal, 0.0f, moveVertical);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown("left shift"))
+        {
+            speed = speed * 2;
+        }
+
+        if (Input.GetKeyUp("left shift"))
+        {
+            speed = 10.0f;
+        }
     }
 }
